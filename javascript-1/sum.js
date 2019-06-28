af63841574c3570b6bae8b6d01396337ffd99497
Eugene Way <eugeneway@MBP-Eugene.home>
2019-06-28 22:15:57 +0200

    add another function type sum()()()

--- javascript-1/sum.js
+++ javascript-1/sum.js
@@ -22,3 +22,20 @@ const sum = makeSum();
 sum(1,2,4,5,6,6,78); //102
 sum(1,3,-2,0); //2
 sum(); //104 Выводит сумму всех переданных до этого значений
+
+
+// Вариант с поочередный вызовом вида sum(a)(b)(n)()
+
+const sum2 = (a) => {
+    return (b) => {
+        if (b) {
+            return sum2(a + b)
+        } else {
+            return a
+        }    
+    }
+}
+
+console.log(sum2(2)(10)(3)(-3)(34)()) //46
+console.log(sum2(2)(10)()) //12
+console.log(sum2()()) //undefined
