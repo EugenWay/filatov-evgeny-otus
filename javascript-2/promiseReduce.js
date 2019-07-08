454dfbe946f9bfa0b9b35efbac7eaf089bd98e4f
Eugene Way <eugeneway@MBP-Eugene.home>
2019-07-08 13:31:38 +0200

    code refactoring and stylistic fix

--- javascript-2/promiseReduce.js
+++ javascript-2/promiseReduce.js
@@ -1,48 +1,36 @@
-const promise1 = () => {
+
+const promiseTemplate = (funcNumber, value) => {
     return new Promise((resolve, reject) => {
-        let daley = Math.ceil(Math.random() * 2000)
+        let delay = Math.ceil(Math.random() * 2000)
         setTimeout(() => {
-            console.info(`Doing async func 1. My time for complete is ${daley}`)
-            resolve(5)
-        }, daley)
+            console.info(`Doing async func ${funcNumber}. My time for complete is ${delay}`)
+            resolve(value)
+        }, delay)
     })
 }
 
-const promise2 = () => {
-    return new Promise((resolve, reject) => {
-        let daley = Math.ceil(Math.random() * 2000)
-        setTimeout(() => {
-            console.info(`Doing async func 2. My time for complete is ${daley}`)
-            resolve(10)
-        }, daley)
-    })
-}
+const p1 = promiseTemplate(1, 5)
+const p2 = promiseTemplate(2, 10)
+const p3 = promiseTemplate(3, 5)
 
-const promise3 = () => {
-    return new Promise((resolve, reject) => {
-        let daley = Math.ceil(Math.random() * 2000)
-        setTimeout(() => {
-            console.info(`Doing async func 3. My time for complete is ${daley}`)
-            resolve(5)
-        }, daley)
-    })
-}
-
-const promises = [promise1, promise2, promise3]
-
+const promises = [p1, p2, p3]
 
 async function promiseReduce(promises, reduce, initialValue) {
-    let result = initialValue;
+    let result = initialValue
 
     for (let promise of promises) {
-        await promise().then(value => result = reduce(result, value));
+        await promise.then(value => result = reduce(result, value))
     }
-    return result;
+    return result
 }
 
-
 promiseReduce(promises, (acc, x) => acc += x, 25)
-    .then(value => console.log(value));
+    .then(value => console.log(value))
+
+
+// Doing async func 1. My time for complete is 1726
+// Doing async func 2. My time for complete is 612
+// Doing async func 3. My time for complete is 1296
 
 // Doing async func 1. My time for complete is 1726
 // Doing async func 2. My time for complete is 612
